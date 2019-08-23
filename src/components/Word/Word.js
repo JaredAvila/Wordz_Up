61f0253c96ce2ca8a2feb217ec451d08b1f290a6
Jared Avila <jared.c.avila@gmail.com>
2019-08-23 09:47:15 -0700

    setup environment variables and installed axios to make api calls

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import { Link } from "react-router-dom";
-
-const DICTIONARY_KEY = process.env.REACT_APP_DICTIONARY_API_KEY;
-const THESAURUS_KEY = process.env.REACT_APP_THESAURUS_API_KEY;
 
 // https://www.dictionaryapi.com/api/v3/references/collegiate/json/cheese?key=your-api-key  DICTIONARY EX
 // https://media.merriam-webster.com/soundc11/[subdirectory]/[base filename].wav  PRONOUNCIATION EX
@@ -15,8 +13,17 @@ class Word extends Component {
   };
 
   componentDidMount() {
+    const DICTIONARY_KEY = process.env.REACT_APP_DICTIONARY_API_KEY;
+    const THESAURUS_KEY = process.env.REACT_APP_THESAURUS_API_KEY;
     const { word } = this.props.match.params;
     this.setState({ word: word });
+    axios
+      .get(
+        `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${
+          this.props.match.params.word
+        }?key=${DICTIONARY_KEY}`
+      )
+      .then(data => console.log(data.data[0]));
   }
 
   render() {
