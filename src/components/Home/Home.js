9cc9cb7adc5e478bf9d6f52bc46b094a4854669c
Jared Avila <jared.c.avila@gmail.com>
2019-08-22 19:20:47 -0700

    working out the routing logic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import * as styles from "./Home.module.css";
 
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleUp } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Home = () => {
-  return (
-    <div className={styles.Home}>
-      <div className={styles.Headline}>
-        <h1 className={styles.Title}>
-          Wordz
-          <FontAwesomeIcon icon={faArrowAltCircleUp} className={styles.Icon} />
-        </h1>
-        <p className={styles.Subline}>Where we go to look Wordz-Up</p>
+class Home extends Component {
+  state = {
+    redirect: false
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("it works!");
+    this.setState({ redirect: true });
+  };
+  render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/word" />;
+    }
+    return (
+      <div className={styles.Home}>
+        <div className={styles.Headline}>
+          <h1 className={styles.Title}>
+            Wordz
+            <FontAwesomeIcon
+              icon={faArrowAltCircleUp}
+              className={styles.Icon}
+            />
+          </h1>
+          <p className={styles.Subline}>Where we go to look Wordz-Up</p>
+        </div>
+        <div className={styles.Search}>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" />
+            <button type="submit">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </form>
+        </div>
       </div>
-      <div className={styles.Search}>
-        <input type="text" />
-        <Link to="/word">Search</Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
