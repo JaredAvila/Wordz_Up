4a812f33c633e5022415bbcf79f5e50f7e9e3bb9
Jared Avila <jared.c.avila@gmail.com>
2019-08-26 12:59:37 -0700

    organizing/parsing JSON data

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
 
 import { Link } from "react-router-dom";
@@ -9,30 +9,73 @@ import { Link } from "react-router-dom";
 
 class Word extends Component {
   state = {
-    word: null
+    word: {
+      spelling: null,
+      type: null,
+      pronounce: null,
+      sound: null
+    },
+    suggestions: [],
+    notFound: false
   };
 
   componentDidMount() {
     const DICTIONARY_KEY = process.env.REACT_APP_DICTIONARY_API_KEY;
     const THESAURUS_KEY = process.env.REACT_APP_THESAURUS_API_KEY;
-    const { word } = this.props.match.params;
-    this.setState({ word: word });
     axios
       .get(
         `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${
           this.props.match.params.word
         }?key=${DICTIONARY_KEY}`
       )
-      .then(data => console.log(data.data[0]));
+      .then(data => {
+        if (typeof data.data[0] !== "object") {
+          console.log("not a word");
+          this.setState({
+            suggestions: data.data,
+            notFound: true
+          });
+        } else {
+          const newWord = {
+            spelling: this.props.match.params.word,
+            type: data.data[0].fl,
+            pronounce: data.data[0].hwi.prs[0].mw,
+            sound: data.data[0].hwi.prs[0].sound.audio
+          };
+          this.setState({
+            word: newWord
+          });
+        }
+        console.log("What I want ", data.data);
+        console.log(data.data[0]);
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.state.word} Component</h1>
+    let markUp = (
+      <Fragment>
+        <h1>
+          {this.state.word.spelling} Component ({this.state.word.type})
+        </h1>
         <Link to="/">Back</Link>
-      </div>
+      </Fragment>
     );
+
+    if (this.state.notFound) {
+      markUp = (
+        <Fragment>
+          <h1>Word Not Found.</h1>
+          <p>Did you mean...</p>
+          <ul>
+            {this.state.suggestions.map(el => {
+              return <li>{el}</li>;
+            })}
+          </ul>
+        </Fragment>
+      );
+    }
+    return <div>{markUp}</div>;
   }
 }
 
